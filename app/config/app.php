3f802a6f1b42d5c10b15171d1dd3fc9dffde2bef
Luracast <arul@luracast.com>
2014-05-16 16:39:30 +0800

    Basic Structure

---
+++ app/config/app.php
@@ -0,0 +1,40 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Autoloaded Service Providers
+	|--------------------------------------------------------------------------
+	|
+	| The service providers listed here will be automatically loaded on the
+	| request to your application. Feel free to add your own services to
+	| this array to grant expanded functionality to your applications.
+	|
+	*/
+
+	'providers' => array(
+		'Illuminate\Database\MigrationServiceProvider',
+		'Illuminate\Database\SeedServiceProvider',
+	),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Class Aliases
+	|--------------------------------------------------------------------------
+	|
+	| This array of class aliases will be registered when this application
+	| is started. However, feel free to register as many as you wish as
+	| the aliases are "lazy" loaded so they don't hinder performance.
+	|
+	*/
+
+	'aliases' => array(
+		'DB'                => 'Illuminate\Database\DatabaseManager',
+		'Eloquent'          => 'Illuminate\Database\Eloquent\Model',
+		'Schema'            => 'Illuminate\Support\Facades\Schema',
+		'Seeder'            => 'Illuminate\Database\Seeder',
+		'SoftDeletingTrait' => 'Illuminate\Database\Eloquent\SoftDeletingTrait',
+	),
+
+);
---
+++ public/index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Laravel Database - for any web application
+ *
+ * @package  Database
+ * @author   Arul Kumaran <arul@luracast.com>
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+require __DIR__.'/../bootstrap/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Configure your Web Application
+|--------------------------------------------------------------------------
+|
+| Configure your favourite web app framework to handle web requests and
+| respond back. If you are using Restler framework, you may simply uncomment
+| the lines below
+|
+*/
+
+/*
+$r = new Restler();
+$r->addApiClass('Users')
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can simply call the run method,
+| which will execute the request and send the response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have whipped up for them.
+|
+*/
+
+// $r->handle();
