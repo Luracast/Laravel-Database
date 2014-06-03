eca6bf7010ffc5844f386af23ceb8d9c17c79192
Luracast <arul@luracast.com>
2014-06-03 23:44:00 +0800

    Environment detection for both CLI and Web Application mode added

--- bootstrap/Container/Container.php
+++ bootstrap/Container/Container.php
@@ -3,9 +3,60 @@ namespace Bootstrap\Container;
 
 class Container extends \Illuminate\Container\Container
 {
+    /**
+     * Detect the application's current environment.
+     *
+     * @param  array|string $environments
+     *
+     * @return string
+     */
+    public function detectEnvironment($environments)
+    {
+        $args = isset($_SERVER['argv']) ? $_SERVER['argv'] : null;
+        if (php_sapi_name() == 'cli' && !is_null($value = $this->getEnvironmentArgument($args))) {
+            //running in console and env param is set
+            return $this['env'] = head(array_slice(explode('=', $value), 1));
+        } else {
+            //running as the web app
+
+            if ($environments instanceof Closure) {
+                // If the given environment is just a Closure, we will defer the environment check
+                // to the Closure the developer has provided, which allows them to totally swap
+                // the webs environment detection logic with their own custom Closure's code.
+                return $this['env'] = call_user_func($environments);
+            } elseif (is_array($environments)) {
+                foreach ($environments as $environment => $hosts) {
+                    // To determine the current environment, we'll simply iterate through the possible
+                    // environments and look for the host that matches the host for this request we
+                    // are currently processing here, then return back these environment's names.
+                    foreach ((array)$hosts as $host) {
+                        if (str_is($host, gethostname())) return $this['env'] = $environment;
+                    }
+                }
+            } elseif (is_string($environments)) {
+                return $this['env'] = $environments;
+            }
+        }
+        return $this['env'] = 'production';
+    }
+
+    /**
+     * Get the environment argument from the console.
+     *
+     * @param  array $args
+     *
+     * @return string|null
+     */
+    private function getEnvironmentArgument(array $args)
+    {
+        return array_first($args, function ($k, $v) {
+            return starts_with($v, '--env');
+        });
+    }
+
     public function environment()
     {
-        return 'local';
+        return $this['env'];
     }
 
     /**
