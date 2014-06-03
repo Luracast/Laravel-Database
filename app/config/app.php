f127e92e2dea5a225cadf6cb9202d1a3b9f8cf67
Luracast <arul@luracast.com>
2014-06-03 21:12:00 +0800

    code formatting

--- app/config/app.php
+++ app/config/app.php
@@ -30,7 +30,7 @@ return array(
 	*/
 
 	'aliases' => array(
-        'DB'                => 'Illuminate\Database\Capsule\Manager',
+		'DB'                => 'Illuminate\Database\Capsule\Manager',
 		'Eloquent'          => 'Illuminate\Database\Eloquent\Model',
 		'Schema'            => 'Illuminate\Support\Facades\Schema',
 		'Seeder'            => 'Illuminate\Database\Seeder',
