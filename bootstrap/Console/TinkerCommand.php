dc068e7cb8c53357197e7f0a4024257bf3e22565
Luracast <arul@luracast.com>
2016-05-14 16:00:18 +0800

    updated tinker command

--- bootstrap/Console/TinkerCommand.php
+++ bootstrap/Console/TinkerCommand.php
@@ -1,23 +1,31 @@
 <?php namespace Bootstrap\Console;
 
+use Psy\Shell;
+use Psy\Configuration;
 use Illuminate\Console\Command;
-
-class TinkerCommand extends Command {
-
+use Symfony\Component\Console\Input\InputArgument;
+class TinkerCommand extends Command
+{
+	/**
+	 * artisan commands to include in the tinker shell.
+	 *
+	 * @var array
+	 */
+	protected $commandWhitelist = [
+		'clear-compiled', 'down', 'env', 'inspire', 'migrate', 'optimize', 'up',
+	];
 	/**
 	 * The console command name.
 	 *
 	 * @var string
 	 */
 	protected $name = 'tinker';
-
 	/**
 	 * The console command description.
 	 *
 	 * @var string
 	 */
-	protected $description = "Interact with your application";
-
+	protected $description = 'Interact with your application';
 	/**
 	 * Execute the console command.
 	 *
@@ -25,100 +33,53 @@ class TinkerCommand extends Command {
 	 */
 	public function fire()
 	{
-		if ($this->supportsBoris())
-		{
-			$this->runBorisShell();
-		}
-		else
-		{
-			$this->comment('Full REPL not supported. Falling back to simple shell.');
-
-			$this->runPlainShell();
-		}
+		$this->getApplication()->setCatchExceptions(false);
+		$config = new Configuration;
+		$config->getPresenter()->addCasters(
+			$this->getCasters()
+		);
+		$shell = new Shell($config);
+		$shell->addCommands($this->getCommands());
+		$shell->setIncludes($this->argument('include'));
+		$shell->run();
 	}
-
 	/**
-	 * Run the Boris REPL with the current context.
+	 * Get artisan commands to pass through to PsySH.
 	 *
-	 * @return void
+	 * @return array
 	 */
-	protected function runBorisShell()
+	protected function getCommands()
 	{
-		$this->setupBorisErrorHandling();
-
-		with(new \Boris\Boris('> '))->start();
-	}
-
-	/**
-	 * Setup the Boris exception handling.
-	 *
-	 * @return void
-	 */
-	protected function setupBorisErrorHandling()
-	{
-		restore_error_handler(); restore_exception_handler();
-
-		$this->laravel->make('artisan')->setCatchExceptions(false);
-
-		$this->laravel->error(function() { return ''; });
-	}
-
-	/**
-	 * Run the plain Artisan tinker shell.
-	 *
-	 * @return void
-	 */
-	protected function runPlainShell()
-	{
-		$input = $this->prompt();
-
-		while ($input != 'quit')
-		{
-			// We will wrap the execution of the command in a try / catch block so we
-			// can easily display the errors in a convenient way instead of having
-			// them bubble back out to the CLI and stop the entire command loop.
-			try
-			{
-				if (starts_with($input, 'dump '))
-				{
-					$input = 'var_dump('.substr($input, 5).');';
-				}
-
-				eval($input);
+		$commands = [];
+		foreach ($this->getApplication()->all() as $name => $command) {
+			if (in_array($name, $this->commandWhitelist)) {
+				$commands[] = $command;
 			}
-
-			// If an exception occurs, we will just display the message and keep this
-			// loop going so we can keep executing commands. However, when a fatal
-			// error occurs, we have no choice but to bail out of this routines.
-			catch (\Exception $e)
-			{
-				$this->error($e->getMessage());
-			}
-
-			$input = $this->prompt();
 		}
+		return $commands;
 	}
-
 	/**
-	 * Prompt the developer for a command.
+	 * Get an array of Laravel tailored casters.
 	 *
-	 * @return string
+	 * @return array
 	 */
-	protected function prompt()
+	protected function getCasters()
 	{
-		$dialog = $this->getHelperSet()->get('dialog');
-
-		return $dialog->ask($this->output, "<info>></info>", null);
+		return [
+			'Illuminate\Foundation\Application' => 'Illuminate\Foundation\Console\IlluminateCaster::castApplication',
+			'Illuminate\Support\Collection' => 'Illuminate\Foundation\Console\IlluminateCaster::castCollection',
+			'Illuminate\Database\Eloquent\Model' => 'Illuminate\Foundation\Console\IlluminateCaster::castModel',
+		];
 	}
-
 	/**
-	 * Determine if the current environment supports Boris.
+	 * Get the console command arguments.
 	 *
-	 * @return bool
+	 * @return array
 	 */
-	protected function supportsBoris()
+	protected function getArguments()
 	{
-		return extension_loaded('readline') && extension_loaded('posix') && extension_loaded('pcntl');
+		return [
+			['include', InputArgument::IS_ARRAY, 'Include file(s) before starting tinker'],
+		];
 	}
-
 }
