32a6a306455d76e5ed7ba9534f638d74d49e18e8
Luracast <arul@luracast.com>
2016-05-14 16:00:55 +0800

    tweaks to artisan

--- bootstrap/Console/Artisan.php
+++ bootstrap/Console/Artisan.php
@@ -23,7 +23,7 @@ class Artisan extends \Illuminate\Console\Application
     {
         if (static::$instance)
             return static::$instance;
-        return static::make()->boot();
+        return static::make();
     }
 
     /**
@@ -39,9 +39,8 @@ class Artisan extends \Illuminate\Console\Application
             /** @var Application $app */
             $app = Facade::getFacadeApplication();
             /** @var Artisan $console */
-            $console = with($console = new static('Laravel Database', '4.2.*'))
-                ->setLaravel($app)
-                ->setExceptionHandler($app['exception'])
+            with($console = new static($app, $app['events'], '5.2.*'))
+                //->setExceptionHandler($app['exception'])
                 ->setAutoExit(false);
 
             $app->instance('artisan', $console);
@@ -50,7 +49,6 @@ class Artisan extends \Illuminate\Console\Application
             $console->add(new ServeCommand());
             $console->add(new ModelMakeCommand($app['files']));
             $console->add(new CommandMakeCommand($app['files']));
-            $console->add(new EnvironmentCommand());
             $console->add(new EnvironmentCommand());
             static::$instance = $console;
         }
