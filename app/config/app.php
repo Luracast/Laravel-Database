89ba1a539862ba90be00c6698fd498a3f5c9e47c
Luracast <arul@luracast.com>
2016-05-18 22:28:54 +0800

    updated configuration for backward compatibility, and adding aliases for Redis and Queue

--- app/config/app.php
+++ app/config/app.php
@@ -1,45 +1,56 @@
 <?php
 
-return array(
+return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Autoloaded Service Providers
-	|--------------------------------------------------------------------------
-	|
-	| The service providers listed here will be automatically loaded on the
-	| request to your application. Feel free to add your own services to
-	| this array to grant expanded functionality to your applications.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
 
-	'providers' => array(
-		'Illuminate\Database\MigrationServiceProvider',
-		'Illuminate\Database\SeedServiceProvider',
-	),
+    'providers' => [
+        'Illuminate\Database\MigrationServiceProvider',
+        'Illuminate\Database\SeedServiceProvider',
+        /*
+         * To add Redis support,
+         *     - run composer require illuminate/redis:5.2.*
+         *     - uncomment the line below
+         */
+        // 'Illuminate\Redis\RedisServiceProvider',
+    ],
 
-	/*
-	|--------------------------------------------------------------------------
-	| Class Aliases
-	|--------------------------------------------------------------------------
-	|
-	| This array of class aliases will be registered when this application
-	| is started. However, feel free to register as many as you wish as
-	| the aliases are "lazy" loaded so they don't hinder performance.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
 
-	'aliases' => array(
-		'DB'                => 'Illuminate\Database\Capsule\Manager',
-		'Eloquent'          => 'Illuminate\Database\Eloquent\Model',
-		'Schema'            => 'Illuminate\Support\Facades\Schema',
-		'Seeder'            => 'Illuminate\Database\Seeder',
-		'SoftDeletingTrait' => 'Illuminate\Database\Eloquent\SoftDeletingTrait',
-		'Artisan'           => 'Bootstrap\Console\ArtisanFacade',
-		'Config'            => 'Luracast\Config\Config',
-		'Cache'             => 'Illuminate\Support\Facades\Cache',
-		'File'              => 'Illuminate\Support\Facades\File',
-		'Event'             => 'Illuminate\Support\Facades\Event',
-	),
+    'aliases' => [
+        'DB'                                             => 'Illuminate\Database\Capsule\Manager',
+        'Eloquent'                                       => 'Illuminate\Database\Eloquent\Model',
+        'Schema'                                         => 'Illuminate\Support\Facades\Schema',
+        'Seeder'                                         => 'Illuminate\Database\Seeder',
+        'SoftDeletingTrait'                              => 'Illuminate\Database\Eloquent\SoftDeletingTrait',
+        'Artisan'                                        => 'Bootstrap\Console\ArtisanFacade',
+        'Config'                                         => 'Luracast\Config\Config',
+        'Cache'                                          => 'Illuminate\Support\Facades\Cache',
+        'File'                                           => 'Illuminate\Support\Facades\File',
+        'Event'                                          => 'Illuminate\Support\Facades\Event',
+        'Redis'                                          => 'Illuminate\Support\Facades\Redis',
+        'Queue'                                          => 'Illuminate\Support\Facades\Queue',
 
-);
+        //backward compatibility for 4.2.x Models
+        'Illuminate\Database\Eloquent\SoftDeletingTrait' => 'Illuminate\Database\Eloquent\SoftDeleting'
+    ],
+
+];
