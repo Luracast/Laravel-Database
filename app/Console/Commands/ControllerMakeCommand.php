68798b156fc4c2772e2d22309cfaaf3ed4110cea
Arul <arul@luracast.com>
2021-08-09 19:51:37 +0800

    Updates pagination logic to work with latest restler

--- app/Console/Commands/ControllerMakeCommand.php
+++ app/Console/Commands/ControllerMakeCommand.php
@@ -98,7 +98,9 @@ class ControllerMakeCommand extends GeneratorCommand
         $replace["use {$controllerNamespace}\Controller;\n"] = '';
 
         return str_replace(
-            array_keys($replace), array_values($replace), parent::buildClass($name)
+            array_keys($replace),
+            array_values($replace),
+            parent::buildClass($name)
         );
     }
 
